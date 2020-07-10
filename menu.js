354d528c6fc47ce87bc6e0312ebd0a5af1e9ec4e
Danaglz <daguilarglz97@gmail.com>
2020-07-10 00:29:57 -0500

    menu

--- menu.js
+++ menu.js
@@ -14,7 +14,7 @@ function main() {
         } else {
             contador = 1;
             $('nav').animate({
-                left: '-200'
+                left: '-400'
             });
         }
     });
