6d72f6715d6edb38714c8d60adddf4b694a8c997
McGilfordJose <chucho970407@gmail.com>
2020-07-09 23:53:43 -0500

    newer

--- js/update-body.js
+++ js/update-body.js
@@ -5,9 +5,13 @@ function change_background()
     minuto = momentoActual.getMinutes();
     segundo = momentoActual.getSeconds();
     console.log(hora+":"+minuto+":"+segundo);
-    if(minuto==36)
+    console.log(segundo%10);
+    if(segundo%10==0)
     {
     document.body.style.backgroundImage = "linear-gradient(to bottom right, rgba(215, 155, 55,0.6) , rgba(151, 51, 255,0.6))";
+    }
+    else{
+        document.body.style.backgroundImage = "linear-gradient(to bottom right, rgb(0, 0, 0) , rgb(89, 89, 255))";
     }
     setTimeout("change_background()",1000)
 }
